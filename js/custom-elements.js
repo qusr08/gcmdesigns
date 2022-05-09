74ea8fba3f7272196bc6a6027767df5cad6baa45
qusr08 <34079525+qusr08@users.noreply.github.com>
2022-05-09 17:44:11 -0400

    added dropdown menus + css animations

--- js/custom-elements.js
+++ js/custom-elements.js
@@ -9,15 +9,26 @@ window.customElements.define('custom-header', class extends HTMLElement {
                 <a href="index.html">gcmdesigns</a>
             </h1>
             <div class="header-list">
-                <a href="about.html">
-                    <p>About</p>
-                </a>
-                <a href="">
-                    <p>Contact</p>
-                </a>
-                <a href="">
-                    <p>Projects</p>
-                </a>
+                <a href="about.html"><p>About</p></a>
+                <div class="dropdown">
+                    <a><p>Contact</p></a>
+                    <div class="dropdown-list">
+                        <a>Resume</a>
+                        <a href="mailto:gcolonmelendez@vt.edu">Email</a>
+                        <a href="https://www.instagram.com/gcmdesigns/">Instagram</a>
+                        <a href="https://www.linkedin.com/in/gabrielacolonmelendez">LinkedIn</a>
+                    </div>
+                </div>
+                <div class="dropdown">
+                    <a><p>Projects</p></a>
+                    <div class="dropdown-list">
+                        <a href="">Zona</a>
+                        <a href="">Community Cycle</a>
+                        <a href="">MelOrganic</a>
+                        <a href="">CLC Luminary</a>
+                        <a href="">Roanoke Follies</a>
+                    </div>
+                </div>
             </div>
         `;
     }
