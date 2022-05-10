143cec5b24dab87c4202dd4a470bf85ea7d453b7
qusr08 <34079525+qusr08@users.noreply.github.com>
2022-05-10 02:18:07 -0400

    added new project pages and changes to header

--- js/custom-elements.js
+++ js/custom-elements.js
@@ -7,11 +7,16 @@ window.customElements.define('custom-header', class extends HTMLElement {
         this.innerHTML = `
             <h1 class="header-title"><a href="index.html">gcmdesigns</a></h1>
             <div class="header-list">
-                <a href="about.html"><p>About</p></a>
+                <div class="dropdown">
+                    <a><p>About</p></a>
+                    <div class="dropdown-list">
+                        <a>Resume</a>
+                        <a href="about.html">Bio</a>
+                    </div>
+                </div>
                 <div class="dropdown">
                     <a><p>Contact</p></a>
                     <div class="dropdown-list">
-                        <a>Resume</a>
                         <a href="mailto:gcolonmelendez@vt.edu">Email</a>
                         <a href="https://www.instagram.com/gcmdesigns/">Instagram</a>
                         <a href="https://www.linkedin.com/in/gabrielacolonmelendez">LinkedIn</a>
@@ -20,11 +25,13 @@ window.customElements.define('custom-header', class extends HTMLElement {
                 <div class="dropdown">
                     <a><p>Projects</p></a>
                     <div class="dropdown-list">
+                        <a href="flatware.html">Flatware</a>
                         <a href="zona.html">Zona</a>
                         <a href="communitycycle.html">Community Cycle</a>
                         <a href="melorganic.html">MelOrganic</a>
                         <a href="luminary.html">CLC Luminary</a>
                         <a href="follies.html">Roanoke Follies</a>
+                        <a href="kirigami.html">Kirigami</a>
                     </div>
                 </div>
             </div>
