f696606abb53bfc256d33ce96914f22cd0581532
qusr08 <34079525+qusr08@users.noreply.github.com>
2022-09-13 20:00:16 -0400

    Added new projects + changes to formatting
    Added mobile-friendly reordering of pictures when screen is smaller
    Projects added: miomed kit and toble
    reorganized some pictures

--- js/custom-elements.js
+++ js/custom-elements.js
@@ -25,10 +25,12 @@ window.customElements.define('custom-header', class extends HTMLElement {
                 <div class="dropdown">
                     <a><p>Projects</p></a>
                     <div class="dropdown-list">
+                        <a href="miomedkit.html">MioMed Kit</a>
+                        <a href="toble.html">Toble</a>
                         <a href="flatware.html">Flatware</a>
                         <a href="zona.html">Zona</a>
                         <a href="communitycycle.html">Community Cycle</a>
-                        <a href="melorganic.html">MelOrganic</a>
+                        <!-- <a href="melorganic.html">MelOrganic</a> -->
                         <a href="luminary.html">CLC Luminary</a>
                         <a href="follies.html">Roanoke Follies</a>
                         <a href="kirigami.html">Kirigami</a>
