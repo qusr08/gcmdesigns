89db61c3d0ca0c75d411ed8df7c09bf5056696f1
qusr08 <34079525+qusr08@users.noreply.github.com>
2022-12-05 06:13:32 -0500

    website 2.0

--- js/custom-elements.js
+++ js/custom-elements.js
@@ -1,48 +1,137 @@
-window.customElements.define('custom-header', class extends HTMLElement {
+window.customElements.define('gcm-project', class extends HTMLElement {
     constructor() {
         super();
+
+        this.gridRowHeight = 0;
     }
 
     connectedCallback() {
-        this.innerHTML = `
-            <h1 class="header-title"><a href="index.html">gcmdesigns</a></h1>
-            <div class="header-list">
-                <div style="position: absolute; bottom: 100%; background-color: var(--color1); width: 100%; height: 150%;"></div>
-                <div class="dropdown">
-                    <a><p>About</p></a>
-                    <div class="dropdown-list">
-                        <a href="media/Resume.pdf">Resume</a>
-                        <a href="about.html">Bio</a>
-                    </div>
-                </div>
-                <div class="dropdown">
-                    <a><p>Contact</p></a>
-                    <div class="dropdown-list">
-                        <a href="mailto:gcolonmelendez@vt.edu">Email</a>
-                        <a href="https://www.instagram.com/gcmdesigns/">Instagram</a>
-                        <a href="https://www.linkedin.com/in/gabrielacolonmelendez">LinkedIn</a>
-                    </div>
-                </div>
-                <div class="dropdown">
-                    <a><p>Projects</p></a>
-                    <div class="dropdown-list">
-                        <a href="miomedkit.html">MioMed Kit</a>
-                        <a href="dos.html">DOS</a>
-                        <a href="toble.html">Toble</a>
-                        <a href="flatware.html">Flatware</a>
-                        <a href="zona.html">Zona</a>
-                        <a href="communitycycle.html">Community Cycle</a>
-                        <!-- <a href="melorganic.html">MelOrganic</a> -->
-                        <a href="luminary.html">CLC Luminary</a>
-                        <a href="follies.html">Follies</a>
-                        <a href="kirigami.html">Kirigami</a>
-                    </div>
-                </div>
-            </div>
-        `;
-    }
-});
+        let content = this.trimAllElements(this.getAttribute('content').split('|'));
 
-customElements.whenDefined('custom-header').then(() => {
-    console.log('custom-header defined');
+        // Content Language Docs
+
+        // elementType:gridColumn: ...
+        //      elementType > How the element is processed and added to the HTML
+        //      gridColumn > The number of columns the element should span (1, 2)
+
+        // image: ... :backgroundImage
+        //      backgroundImage > The path of the background image
+
+        // text: ... :backgroundColor:justifyContent:alignItems:text1:style1:text2:style2: ...
+        //      backgroundColor > The background color of the text
+        //      justifyContent > The vertical alignment of the text items
+        //      alignItems > The horizontal alignment of the text items
+        //      textN > The text to be displayed
+        //      styleN > The style of the text before it (h1, h2, h3, h4, h5, h6, p)
+
+        // embed: ... :file:fileType
+        //      file > The file to embed
+        //      fileType > The type of file to embed
+
+        // iframe: ... :link
+        //      link > The link to the video
+
+        // Loop through all the content and add it to the inner HTML
+        for (let i = 0; i < content.length; i++) {
+            let element = this.trimAllElements(content[i].split('*'));
+
+            // Make sure the element has parameters
+            if (element.length <= 2) {
+                console.log(`Element ${element[0]} with no parameters!`);
+                continue;
+            }
+
+            // Calculate the grid height over time
+            this.gridRowHeight += parseFloat(element[1]) / 2;
+
+            // Remove the base parameters and get only the type specific parameters
+            let elementParameters = element.slice(2);
+
+            switch (element[0]) {
+                case 'image':
+                    let image = document.createElement('span');
+                    image.style.gridColumn = `span ${element[1]}`;
+                    image.style.backgroundImage = `url('${elementParameters[0]}')`;
+                    this.appendChild(image);
+
+                    break;
+
+                case 'text':
+                    let div = document.createElement('div');
+                    div.style.gridColumn = `span ${element[1]}`;
+                    div.style.backgroundColor = elementParameters[0];
+                    div.style.justifyContent = elementParameters[1];
+                    div.style.alignItems = elementParameters[2];
+
+                    for (let j = 3; j < elementParameters.length; j += 2) {
+                        let text = document.createElement(elementParameters[j + 1]);
+                        text.innerHTML += elementParameters[j];
+
+                        // If the text style is set to h3, add a text accent
+                        if (elementParameters[j + 1] == 'h3') {
+                            text.innerHTML += `<span class="text-accent top left" style="border-color: white;"></span>`;
+                        }
+
+                        div.appendChild(text);
+                    }
+
+                    this.appendChild(div);
+
+                    break;
+
+                case 'embed':
+                    let embed = document.createElement('embed');
+                    embed.style.gridColumn = `span ${element[1]}`;
+                    embed.src = elementParameters[0];
+                    embed.type = elementParameters[1];
+                    this.appendChild(embed);
+
+                    break;
+
+                case 'iframe':
+                    let iframe = document.createElement('iframe');
+                    iframe.style.gridColumn = `span ${element[1]}`;
+                    iframe.src = elementParameters[0];
+                    iframe.allowFullscreen = true;
+                    this.appendChild(iframe);
+
+                    break;
+
+                default:
+                    console.log(`Unknown element type '${element[0]}'.`);
+
+                    break;
+            }
+        }
+
+        this.updateGridRowHeight();
+    }
+
+    static get observedAttributes() {
+        return ['class'];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        switch (name) {
+            case 'class':
+                this.updateGridRowHeight();
+                break;
+        }
+    }
+
+    updateGridRowHeight() {
+        if (this.classList.contains('view')) {
+            this.style.gridTemplateRows = `repeat(${Math.ceil(this.gridRowHeight)}, 85vh)`;
+        } else {
+            this.style.gridTemplateRows = `repeat(${Math.ceil(this.gridRowHeight)}, calc(100vh - var(--header-h)))`;
+        }
+    }
+
+    trimAllElements(array) {
+        for (let i = 0; i < array.length; i++) {
+            array[i] = array[i].trim();
+        }
+
+        return array;
+    }
 });
